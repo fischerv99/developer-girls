deca0121c653473b6de124f154c77f70c7bb9a21
JuleBuerkle <jb298@hdm-stuttgart.de>
2024-11-06 11:07:22 +0100

    routen

--- webprojekt/bowlissimo/start/routes.ts
+++ webprojekt/bowlissimo/start/routes.ts
@@ -10,9 +10,18 @@
 import router from '@adonisjs/core/services/router'
 import db from "@adonisjs/lucid/services/db"
 
-router.on('/').render('pages/startseite')
+router.get('/', async ({ view }) => {
+  const pasta = await db.from('pasta').select('*')
+  return view.render('startseite', { pasta}) 
+})
 
-router.get('/startseite1', async ({ view }) => {
-    const pasta = await db.from('pasta').select('*')
-  return view.render('pages/startseite1', { pasta })
+
+router.get('/startseite_drinks', async ({ view }) => {
+  const getränke = await db.from('getränke').select('*') 
+  return view.render('startseite_drinks', { getränke })
+})
+
+router.get('/startseite_beilagen', async ({ view }) => {
+  const beilagen = await db.from('beilagen').select('*') 
+  return view.render('startseite_beilagen', { beilagen })
 })
