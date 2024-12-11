d3cc9b261788ded20c4c5782d1db1aaa880d8821
JuleBuerkle <jb298@hdm-stuttgart.de>
2024-12-11 17:27:45 +0100

    buttons gehen

--- webprojekt/bowlissimo/app/services/clean.js
+++ webprojekt/bowlissimo/app/services/clean.js
@@ -5,7 +5,12 @@
 const Database = use('Database')
 
 
-// Service für das Löschen von alten Warenkörben
+// Löschen von Warenkörben, die nicht bestellt wurden 
+//Löschen von ausgewählten Produkten, die in nicht bestellten Warenkörben liegen
+//Löschen von Kreationen, (die ausgewaehlte Produkte sind) die in nicht bestellten Warenkörben liegen und nicht favorisiert sind
+//Löschen von der Verbindung kreation_toppings, die Kreationen enthalten, die in nicht bestellten Warenkörben liegen und nicht favorisiert sind
+
+
 //Services sind Klassen, die wiederverwendbare Funktionen enthalten. Sie können in Controllern oder anderen Services verwendet werden.
 class CartCleanupService {
   constructor () {
@@ -14,18 +19,24 @@ class CartCleanupService {
   }
 
   async cleanupOldCarts () {
-    try {
-      // Löscht alle nicht bestellten Warenkörbe, die älter als 24 Stunden sind
-      const deleted = await Database
-        .from('warenkorb_bestellung') 
-        .where('in_bestellung', 0)
-        .where('zeitstempel', '<', Database.raw('NOW() - INTERVAL 24 HOUR'))
-        .delete()
+      // Löscht alles, die älter als 24 Stunden sind
+      const gelöschte_warenkörbe =await Database.from('warenkorb_bestellung') 
+                                                .where('in_bestellung', 0)
+                                                .where('zeitstempel', '<', Database.raw('NOW() - INTERVAL 24 HOUR'))
+                                                .delete()
+      
+      const gelöschte_produkte = await Database.from('ausgwwaehltes_produkt')
+                                                .where('warenkorb_id', 'in', gelöschte_warenkörbe)
+                                                .delete()
 
-      console.log(`${deleted} Warenkörbe wurden gelöscht.`)
-    } catch (error) {
-      console.error('Fehler beim Löschen der Warenkörbe:', error)
-    }
+      const gelöschte_kreationen = await Database.from('kreation')
+                                                  .where('id', 'in', gelöschte_produkte)
+                                                  .where('favorisiert', 0)
+                                                  .delete()
+      
+      const gelöschte_kreation_toppings = await Database.from('kreation_toppings')
+                                                        .where('kreation_id', 'in', gelöschte_kreationen)
+
   }
 
   startCleanup () {
